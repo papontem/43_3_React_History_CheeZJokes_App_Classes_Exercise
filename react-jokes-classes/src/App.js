7695f88339ef959797122f45e6d6191590e3ef14
papontemjetix <papontem@gmail.com>
2023-12-23 21:57:22 -0500

    function syntax for app and joke components... todo Jokelist Component

--- react-jokes-classes/src/App.js
+++ react-jokes-classes/src/App.js
@@ -3,14 +3,13 @@ import JokeList from "./JokeList";
 
 /** App component. Renders list of jokes. */
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <JokeList />
-      </div>
-    );
-  }
+function App(props) {
+  // console.log("App running!");
+	return (
+		<div className="App">
+			<JokeList />
+		</div>
+	);
 }
 
 export default App;
